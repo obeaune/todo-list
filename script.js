f50aaa30e0277976a8871170bcd337a9109f049a
Obeaune <linecora86@gmail.com>
2021-11-25 16:18:46 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+// mudar cor de fundo da tarefa ao clicar
+const arrElementoSelected = document.getElementsByClassName('selected');
+function selectItem(event) {
+  if (arrElementoSelected.length > 0) {
+    arrElementoSelected[0].classList.remove('selected');
+  }
+  event.target.classList.toggle('selected');
+}
 // imprimir o texto colocado no input em forma de lista ordenada
 const botaoAdicionar = document.querySelector('#criar-tarefa');
-const idElementoOl = document.querySelector('#lista-tarefas');
+const elementoOl = document.querySelector('#lista-tarefas');
 const input = document.getElementById('texto-tarefa');
 function imprimindoLista() {
   if (input.value !== '') {
     const item = document.createElement('li');
     item.innerHTML = input.value;
-    idElementoOl.appendChild(item);
+    item.addEventListener('click', selectItem);
+    elementoOl.appendChild(item);
     input.value = '';
   }
 }
